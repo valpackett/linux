1f98bcd1508dec3920e2051f88fcc5d4ea3c0b81
Alexey Minnekhanov <alexeymin@postmarketos.org>
2026-01-28 23:16:53 -0300

    HACKY Input: novatek-nvt-ts - demote spammy warning
    On some devices dmesg get spamemd ~10 times per second with:
    
      novatek-nvt-ts 0-0001: slot 14 out of range, ignoring
    
    Demote this print to dev_dbg. This might indicate that
    perhaps firmware update is needed to report correct number of
    touches and cause inconsistencies with ones reported in IRQ?
    
    At the same time report max_touches reported by firmware as
    dev_info so it can be seen in dmesg once.
    
    Signed-off-by: Alexey Minnekhanov <alexeymin@postmarketos.org>
    Signed-off-by: Val Packett <val@packett.cool>

--- drivers/input/touchscreen/novatek-nvt-ts.c
+++ drivers/input/touchscreen/novatek-nvt-ts.c
@@ -110,7 +110,7 @@ static irqreturn_t nvt_ts_irq(int irq, void *dev_id)
 
 		slot = touch[0] >> NVT_TS_TOUCH_SLOT_SHIFT;
 		if (slot < 1 || slot > data->max_touches) {
-			dev_warn(dev, "slot %d out of range, ignoring\n", slot);
+			dev_dbg(dev, "slot %d out of range, ignoring\n", slot);
 			continue;
 		}
 
@@ -268,7 +268,7 @@ static int nvt_ts_probe(struct i2c_client *client)
 		return -EIO;
 	}
 
-	dev_dbg(dev, "Detected %dx%d touchscreen with %d max touches\n",
+	dev_info(dev, "Detected %dx%d touchscreen with %d max touches\n",
 		width, height, data->max_touches);
 
 	if (data->buf[NVT_TS_PARAMS_MAX_BUTTONS])
